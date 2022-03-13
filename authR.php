db94ad0743a02a6d4194b85667ff214127656235
Shafi.git <alsafi315@gmail.com>
2022-03-14 01:22:51 +0600

    Changed the array push in step by step method but no display of error shows

--- authR.php
+++ authR.php
@@ -49,50 +49,51 @@ $error_array=array();
 	$date= date("Y-m-d");
 
 	//checking duplicate mails
-		$e_check = mysqli_query ($con, "SELECT email FROM authr WHERE email='$email'");
+	$e_check = mysqli_query ($con, "SELECT email FROM authr WHERE email='$email'");
 
-		$num_rows = mysqli_num_rows($e_check);
+	$num_rows = mysqli_num_rows($e_check);
 
-		if ($num_rows > 0){
-			array_push($error_array, "Email already in use<br>");
-		}
-		else {
-			array_push($error_array,"Emails don't match<br>");
-		}
-
-	//name conditions
-
-		if(strlen($fname)> 25 || strlen($fname)<2){
-			array_push($error_array,"Your first name should be between 2 and 25 characters<br>");
-		}
-		if (strlen($lname)> 25 || strlen($lname)< 2) {
-			array_push($error_array, "Your last name should be between 2 and 25 characters<br>");
-		}
-
+	if ($num_rows > 0){
+		array_push($error_array, "Email already in use <br>");
+	}
+	else {
+		//name conditions
+		if (strlen($fname)> 25 || strlen($fname)<2){
+			array_push($error_array,"Your first name should be between 2 and 25 characters <br>");
+		}elseif (strlen($lname)> 25 || strlen($lname)< 2) {
+			array_push($error_array, "Your first name should be between 2 and 25 characters <br>");
+		}else{
 	//password conditons
 		if($password != $conpassword){
-			array_push($error_array, "Your passwrod don't match<br>");
-		}
-		else {
+		array_push($error_array, "Your passwrod don't match <br>");
+		 }else {
 			if(preg_match ('/[^A-Za-z0-9]/',$password)){
-				array_push($error_array,"Your password can only contains english characters or numbers<br>");
-		       }
-		     }
-    // paswrod limit
-		if(strlen($password) >30 || strlen($password)<5){
-			array_push($error_array, "Your passwrod must be betweem 5 and 30 characters<br>");
+				array_push($error_array,"Your password can only contains english characters or numbers <br>");
+			   }else{
+				   // paswrod limit
+				   if(strlen($password) >30 || strlen($password)<5){
+					array_push($error_array, "Your passwrod must be betweem 5 and 30 characters <br>");
+				}else{
+					$stmt = $con->prepare("INSERT INTO authr(fname,lname,email,password,date,phone,gender)
+					values(?,?,?,?,?,?,?)");
+					$stmt->bind_param("sssssis",$fname,$lname,$email,$password,$date,$phone,$gender);
+					$stmt->execute();
+					echo "Registered Succesfull";
+					$stmt->close();
+					$con->close();
+					header("location:authR.php");
+					
+				}
+			   }
+			}
 		}
+		
 	}
 
-	// $stmt = $con->prepare("INSERT INTO authr(fname,lname,email,password,date,phone,gender)
-	// values(?,?,?,?,?,?,?)");
-	// $stmt->bind_param("sssssis",$fname,$lname,$email,$password,$date,$phone,$gender);
-	// $stmt->execute();
-	// echo "Registered Succesfull";
-	// $stmt->close();
-	// $con->close();
-	// header("location:authR.php");
-    session_destroy();
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -139,7 +140,7 @@ $error_array=array();
 			echo $_SESSION['phone'];
 		} ?>" required />
 		<br>
-		<!-- <?php if (in_array("Please enter a valid phone number<br>",$error_array)) echo "Please enter a valid phone number<br>"; ?> -->
+	
 
     <input type="button" name="next" class="next action-button" value="Next" />
   </fieldset>
@@ -190,6 +191,9 @@ $error_array=array();
 </div>
 
 <script src="Js/authR.js"> </script>
+<?php
+session_destroy();
+?>
 </body>
 </html>
 
